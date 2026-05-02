57b1c5a905f879f20b87c94b07909fd14c9d7a29
BrendelCraft <mielikuvitusrikas@gmail.com>
2026-05-02 12:01:23 +0200

    02/05/2026

---
+++ header.js
@@ -0,0 +1,25 @@
+//////////////HEADER/////////////////
+const headerContent = 
+`
+
+ <h1>BrendelCraft</h1>
+<nav>
+<a href="index.html">INICIO</a>
+<a href="obras.html">OBRAS</a>
+<a href="quien-soy.html">SOBRE MI</a>
+</nav>
+
+`
+;
+document.getElementById('header-placeholder').innerHTML = headerContent;
+
+
+/////////FOOTER//////////////
+const footerContent = 
+`
+  <footer>
+    <p>Brendelcraft &mdash; <a href="mailto:mielikuvitusrikas@gmail.com">mielikuvitusrikas@gmail.com</a></p>
+  </footer>
+`
+;
+document.getElementById('footer-placeholder').innerHTML = footerContent;
---
+++ obras/headerobras.js
@@ -0,0 +1,23 @@
+//////////////HEADER DE LAS OBRAS/////////////////
+const headerContent = 
+`
+ <h1>BrendelCraft</h1>
+<nav>
+<a href="../index.html">INICIO</a>
+<a href="../obras.html">OBRAS</a>
+<a href="../quien-soy.html">SOBRE MI</a>
+</nav>
+`
+;
+document.getElementById('header-placeholder').innerHTML = headerContent;
+
+
+/////////FOOTER DE LAS OBRAS//////////////
+const footerContent = 
+`
+  <footer>
+    <p>Brendelcraft &mdash; <a href="mailto:mielikuvitusrikas@gmail.com">mielikuvitusrikas@gmail.com</a></p>
+  </footer>
+`
+;
+document.getElementById('footer-placeholder').innerHTML = footerContent;
